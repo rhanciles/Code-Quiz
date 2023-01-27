4aa1fb83048ca16d10da286fd3f83ee7a8c66e49
rhanciles <119437254+rhanciles@users.noreply.github.com>
2023-01-27 12:30:51 +0000

    Move start files to root directory

---
+++ assets/js/logic.js
@@ -0,0 +1,123 @@
+
+// Create target variables.
+var intro = document.querySelector("#start-screen");
+var testPage = document.querySelector("#questions");
+var feedBack = document.querySelector("#feedback");
+var roundUp = document.querySelector("#end-screen");
+var counter = document.querySelector("#time");
+var status = document.querySelectorAll(".start");
+var questionEL= document.querySelector("#question-title");
+var choicesEL=document.querySelector("#choices");
+var finalScore=document.querySelector("#final-score");
+var index = 0;
+var score = 0
+var clickCounter = 0
+var secondsLeft = 50;
+
+// Create timer to control the session,
+function setTime() {
+  // Set interval in variable
+  var timerInterval = setInterval(function() {
+    secondsLeft--;
+    counter.textContent = secondsLeft;
+
+    if(secondsLeft <= 1) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    
+    }
+
+  }, 1000);
+}
+
+// Setup function to create buttons and call the porperties from the questionarr object
+function renderOptions() {
+ 
+  questionEL.textContent=questionsArr[index].question;
+  
+    var button1 = document.createElement("button");
+    var button2 = document.createElement("button");
+    var button3 = document.createElement("button");
+    var button4 = document.createElement("button");
+  
+    button1.setAttribute("class", "choiceList");
+    button2.setAttribute("class", "choiceList");
+    button3.setAttribute("class", "choiceList");
+    button4.setAttribute("class", "choiceList");
+    
+    button1.textContent = questionsArr[index].choice1;
+    button2.textContent = questionsArr[index].choice2;
+    button3.textContent = questionsArr[index].choice3;
+    button4.textContent = questionsArr[index].choice4;
+
+    choicesEL.append(button1, button2, button3, button4);
+  }
+
+// This verifies if the answer is correct and clears the page
+function checkAnswer(answer){
+  console.log(answer, "");
+  if(answer === questionsArr[index].correct){
+    index++
+    score += 1
+    choicesEL.innerHTML = "";
+    feedBack.innerHTML = "Correct";
+    renderOptions();
+
+  } else {
+    index++
+    score -= 1
+    choicesEL.innerHTML = "";
+    feedBack.innerHTML = "Wrong - Ten seconds is taken of your time";
+    secondsLeft = secondsLeft - 10;
+    renderOptions();
+  }
+}
+
+function scores () {
+  testPage.setAttribute("class", "hide");
+  // If the button is clicked while the class is "hide", we set the class to "start"
+  roundUp.setAttribute("class", "start");
+  // If the button is clicked while the class is "hide", we set the class to "start"
+  feedBack.innerHTML = "Register your score";
+  finalScore.innerHTML = score;
+}
+
+// Add event listener to the start button,
+start.addEventListener("click", function(event) {
+    var start = event.target;
+
+  if (start.matches("button") === true) {
+    // Get its data-index value to to apply clauses below
+
+    intro.setAttribute("class", "hide");
+    // If the button is clicked while the class is "start", we set the class to "hide"
+    testPage.setAttribute("class", "start");
+    // If the button is clicked while the class is "hide", we set the class to "start"
+    feedBack.setAttribute("class", "start");
+    // If the button is clicked while the class is "hide", we set the class to "start"
+ 
+    setTime();
+
+    renderOptions();
+     
+      } else {
+       alert = "Please click the button to start";
+    }
+});
+
+  
+// Set event listener to index click options of the user.
+choicesEL.addEventListener("click",()=>{
+  var clicked = this.event.target.textContent;
+  console.log(clicked);
+  checkAnswer(clicked);
+  clickCounter += 1;
+  console.log(clickCounter);
+
+  if (clickCounter >= 4) {
+    scores();
+  } else if (secondsLeft <= 1) {
+    scores();
+  }
+})
+
---
+++ assets/js/questions.js
@@ -0,0 +1,52 @@
+
+// Create an object array of the questions and answers.
+var questionsArr=[
+  {
+    question: "Commonly used data types do not include?",
+    choice1:"Strings",
+    choice2:"Booleans", 
+    choice3:"Alerts",
+    choice4:"Numbers",
+    correct:"Alerts"
+  },
+
+  {
+    question: "A very useful tool used during development and debugging for printing content to the debugger is?",
+    choice1:"JavaScript",
+    choice2:"Terminal/Bash", 
+    choice3:"For Loops", 
+    choice4:"Console Log",
+    correct:"Console Log"
+  },
+
+  {
+    question: "The condition in an if / else statement is enclosed with ________________?",
+    choice1:"Quotes",
+    choice2:"Curley Brackets", 
+    choice3:"Parenthesis",
+    choice4:"Square Brackets",
+    correct:"Curley Brackets"
+  },
+
+  {
+    question: "Arrays in JavaScript can be used to store ________________?",
+    choice1:"Numbers & Strings",
+    choice2:"Booleans",
+    choice3:"Other Arrays",
+    choice4:"All of the above",
+    correct:"All of the above"
+  },
+
+  {
+    question: "Strings values must be enclosed within __________________ when being assigned to variables?",
+    choice1:"Commas", 
+    choice2:"Curly Brackets",
+    choice3:"Quotes",
+    choice4:"Parenthesis",
+    correct:"Quotes"
+  }
+
+]
+
+console.log(questionsArr)
+
